d4c6da8df5127341888f7f100f2857f14f7a32ab
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-06-25 09:28:05 -0300

    create show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
 class ArticlesController < ApplicationController
+  # A frequent practice is to place the standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
     
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+ 
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
